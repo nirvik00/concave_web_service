e54f5c17ba6f1aa9f202c928c5bfc9aadc6f6d62
nirvik00 <nirviksaha@gatech.edu>
2018-02-04 17:17:12 -0500

    adjusted port

--- app.js
+++ app.js
@@ -91,7 +91,7 @@ app.use('/members', members);
 app.use('/users', users);
 
 //start server on port heroku or locally at 3000 
-const port=process.env.port || 3000;
+const port=process.env.PORT || 3000;
 app.listen(port, ()=>{
   console.log('server started on port ${port}');
 });
