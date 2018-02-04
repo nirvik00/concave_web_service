95664b9ccde5ec356b6afee5b80338f5659d163c
nirvik00 <nirviksaha@gatech.edu>
2018-02-04 17:03:15 -0500

    change in spelling mistake

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
-if(process.env.node_env==='production'){
+if(process.env.NODE_ENV==='production'){
   module.exports={mongoURI:'mongodb://nirvik:Nirvik0112358;@ds125058.mlab.com:25058/concave-prod'}
 }else{
   module.exports={mongoURI:'mongodb://localhost/concave-dev'}
