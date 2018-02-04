a5c201d887a7bdaa6ba0b17943c82d4a17c67b7e
nirvik00 <nirviksaha@gatech.edu>
2018-02-04 17:12:45 -0500

    updated password

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV==='production'){
-  module.exports={mongoURI:'mongodb://nirvik:Nirvik0112358;@ds125058.mlab.com:25058/concave-prod'}
+  module.exports={mongoURI:'mongodb://nirvik:Nirvik0112358@ds125058.mlab.com:25058/concave-prod'}
 }else{
   module.exports={mongoURI:'mongodb://localhost/concave-dev'}
 }
